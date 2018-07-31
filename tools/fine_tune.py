7407a704daa31e274247537c2a0ca93615887125
Tijmen <tijmenlv@me.com>
2018-07-31 13:50:48 +0200

    Cleanup, and fixed data_dir bug

--- tools/fine_tune.py
+++ tools/fine_tune.py
@@ -5,17 +5,20 @@ slim = tf.contrib.slim
 def init_weights(scope_name, path):
     if path == None:
         return
+
+    # look for checkpoint
     model_path = tf.train.latest_checkpoint(path)
     initializer_fn = None
+
     if model_path:
+        # only restore variables in the scope_name scope
         variables_to_restore = slim.get_variables_to_restore(include=[scope_name])
         # Create the saver which will be used to restore the variables.
         initializer_fn = slim.assign_from_checkpoint_fn(model_path, variables_to_restore)
     else:
-        print(model_path)
-        print("could not fine fine tune ckpt")
+        print("could not find the fine tune ckpt at {}".format(path))
         exit()
+
     def InitFn(scaffold,sess):
-        if initializer_fn is not None:
-            initializer_fn(sess)
+        initializer_fn(sess)
     return InitFn
